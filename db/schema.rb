962dfd07b4eaa00a25b679506916fd911d0e3ebc
Adamenko <mangamen45@gmail.com>
2016-11-14 07:25:25 +0200

    Add password_digest and create adding users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113224005) do
+ActiveRecord::Schema.define(version: 20161114050301) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -23,9 +23,9 @@ ActiveRecord::Schema.define(version: 20161113224005) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
