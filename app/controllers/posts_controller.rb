7582c8606701192bd04a804a1462ecf70a2fd66f
Adamenko <mangamen45@gmail.com>
2016-11-17 15:51:50 +0200

    Add column username to User model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_coockie_param
 
   # GET /posts
   # GET /posts.json
@@ -66,6 +66,12 @@ class PostsController < ApplicationController
   end
 
   private
+    def set_coockie_param
+      if current_user
+        user_session[:counter_up_to_ten] ||= 0
+      end
+    end
+
     def def_respond_to(post)
       respond_to do |format|
         format.json {
