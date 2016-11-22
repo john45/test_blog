81a5400c2f6ee050b11c599fa2aa392701b99ded
Adamenko <mangamen45@gmail.com>
2016-11-22 07:53:42 +0200

    added static pages Home Help and About

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
-
+  get 'static_pages/about'
   get 'static_pages/help'
 
   devise_for :users, controllers: {
       registrations: 'users/registrations'
   }
   resources :posts
-  root 'posts#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
