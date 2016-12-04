703fe838950cbca6df075a1eda7852d2ab5385ee
Adamenko <mangamen45@gmail.com>
2016-12-04 21:33:39 +0200

    Remove email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   before_save {self.email = email.downcase}
-  after_create :welcome_message
+  # after_create :welcome_message
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: {minimum: 2, maximum: 50}
